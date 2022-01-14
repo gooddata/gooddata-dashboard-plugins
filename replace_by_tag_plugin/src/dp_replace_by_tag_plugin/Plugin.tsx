6cf8ca233a0beb684967248e2f0c0a48cbb9d568
Martin Plavek <martin.plavek@gooddata.com>
2022-01-14 14:18:12 +0100

    Plugin.tsx documentation

--- replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Plugin.tsx
+++ replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Plugin.tsx
@@ -17,13 +17,19 @@ export class Plugin extends DashboardPluginV1 {
     public readonly minEngineVersion = entryPoint.minEngineVersion;
     public readonly maxEngineVersion = entryPoint.maxEngineVersion;
 
+    /**
+     * Tags define by plugin to be replaced.
+     */
     public tags: string[] = [];
 
     public onPluginLoaded(
         _ctx: DashboardContext,
         parameters?: string
     ): Promise<void> | void {
-        
+        /**
+         * Run the `link-plugin` command with `--with-parameters` flag and enter all the tags you want to replace with
+         * `GaugeChart` separated by space. By default all bullet charts with tag `gauge` will be replaced.
+         */
         this.tags = parameters?.split(" ") || ["gauge"];
     }
 
@@ -33,13 +39,19 @@ export class Plugin extends DashboardPluginV1 {
         _handlers: IDashboardEventHandling
     ): void {
         customize.insightWidgets().withCustomProvider((insight) => {
+            /**
+             * If at least one tag from plugin parameters (or `gauge` tag) is present in the tags of the insight
+             * and the type of the insight is bullet chart, replace this insight with GaugeAdapter component.
+             */
             if (
                 insightTags(insight).some(insightTag => this.tags.includes(insightTag)) &&
                 insightVisualizationUrl(insight).includes("bullet")
             ) {
                 return GaugeAdapter;
             }
-
+            /**
+             * If undefined is returned, nothing happens and original component stays in place.
+             */
             return undefined;
         });
     }
