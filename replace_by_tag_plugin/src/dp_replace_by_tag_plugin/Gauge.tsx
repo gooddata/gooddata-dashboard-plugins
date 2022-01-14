7773d020269ed960a87540fa60853b2064c14be6
Dan Homola <dan.homola@gooddata.com>
2022-01-14 11:39:23 +0100

    First functioning prototype

--- replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Gauge.tsx
+++ replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Gauge.tsx
@@ -1,40 +1,70 @@
 // (C) 2022 GoodData Corporation
+import { DataValue } from "@gooddata/sdk-backend-spi";
+import { bucketMeasure, insightBucket } from "@gooddata/sdk-model";
+import { ErrorComponent, LoadingComponent } from "@gooddata/sdk-ui";
 import { CustomDashboardInsightComponent } from "@gooddata/sdk-ui-dashboard";
 import React from "react";
-import {useInsightWidgetDataView} from "./utils/useInsightWidgetDataView";
+import { useInsightWidgetDataView } from "./utils/useInsightWidgetDataView";
+
+function parseResult(value: DataValue): number {
+    return Number.parseFloat(value?.toString() ?? "0");
+}
 
 export const GaugeAdapter: CustomDashboardInsightComponent = (props) => {
-    // TODO get the data for props.insight
     const {
-        ErrorComponent: GaugeError,
-        LoadingComponent: GaugeLoading,
-        widget
+        ErrorComponent: CustomError,
+        LoadingComponent: CustomLoading,
+        widget,
+        insight,
     } = props;
 
-    const {result, error, status} = useInsightWidgetDataView({
-        insightWidget: widget
+    const GaugeError = CustomError ?? ErrorComponent;
+    const GaugeLoading = CustomLoading ?? LoadingComponent;
+
+    const { result, error, status } = useInsightWidgetDataView({
+        insightWidget: widget,
     });
 
-    if((status === "loading" || status === "pending") && GaugeLoading) {
-        return <GaugeLoading />
-    }
-    if(error && GaugeError) {
-        return <GaugeError message={error.message}/>
+    if (status === "loading" || status === "pending") {
+        return <GaugeLoading />;
     }
 
-    console.log("result", result);
-    if(result) {
-        const [min, max, value] = result.data().series().toArray()[0].dataPoints();
-
-        return <Gauge min={min.rawValue()} max={max} value={value} />
+    if (status === "error") {
+        return <GaugeError message={error?.message ?? "Unknown error"} />;
     }
-    return <GaugeError message={"Result is empty"}/>
+
+    const primaryMeasureBucket = insightBucket(insight, "measures");
+    const secondaryMeasureBucket = insightBucket(insight, "secondary_measures");
+
+    if (!primaryMeasureBucket || !secondaryMeasureBucket) {
+        return <GaugeError message={"Insight buckets are weird"} />;
+    }
+
+    const primaryMeasure = bucketMeasure(primaryMeasureBucket)!;
+    const secondaryMeasure = bucketMeasure(secondaryMeasureBucket)!;
+
+    const value = result!
+        .data()
+        .series()
+        .firstForMeasure(primaryMeasure)
+        .dataPoints()[0];
+    const max = result!
+        .data()
+        .series()
+        .firstForMeasure(secondaryMeasure)
+        .dataPoints()[0];
+
+    return (
+        <Gauge
+            max={parseResult(max.rawValue)}
+            value={parseResult(value.rawValue)}
+        />
+    );
 };
 
 export const Gauge: React.FC<{
-    min: number;
     max: number;
     value: number;
-}> = () => {
-    return <div>GAUGE</div>
+}> = ({ max, value }) => {
+    return <div>GAUGE {[max, value].join(", ")}</div>;
 };
