93b7f6ed91b238674fb7f45012ffbcdacc0fef42
Martin Plavek <martin.plavek@gooddata.com>
2022-01-14 11:13:47 +0100

    GaugeAdapter execution impl

--- replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Gauge.tsx
+++ replace_by_tag_plugin/src/dp_replace_by_tag_plugin/Gauge.tsx
@@ -1,10 +1,34 @@
 // (C) 2022 GoodData Corporation
 import { CustomDashboardInsightComponent } from "@gooddata/sdk-ui-dashboard";
 import React from "react";
+import {useInsightWidgetDataView} from "./utils/useInsightWidgetDataView";
 
-export const GaugeAdapter: CustomDashboardInsightComponent = (_props) => {
+export const GaugeAdapter: CustomDashboardInsightComponent = (props) => {
     // TODO get the data for props.insight
-    return <Gauge min={0} max={100} value={80} />
+    const {
+        ErrorComponent: GaugeError,
+        LoadingComponent: GaugeLoading,
+        widget
+    } = props;
+
+    const {result, error, status} = useInsightWidgetDataView({
+        insightWidget: widget
+    });
+
+    if((status === "loading" || status === "pending") && GaugeLoading) {
+        return <GaugeLoading />
+    }
+    if(error && GaugeError) {
+        return <GaugeError message={error.message}/>
+    }
+
+    console.log("result", result);
+    if(result) {
+        const [min, max, value] = result.data().series().toArray()[0].dataPoints();
+
+        return <Gauge min={min.rawValue()} max={max} value={value} />
+    }
+    return <GaugeError message={"Result is empty"}/>
 };
 
 export const Gauge: React.FC<{
