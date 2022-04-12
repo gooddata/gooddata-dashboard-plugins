da0f95e6d9b35492d3663fa4942a97ceecc7ac23
Martin Plavek <martin.plavek@gooddata.com>
2022-04-12 12:51:46 +0200

    Remove workspace settings and fixtures
    JIRA: RAIL-4180

--- tooltip_plugin/src/dp_tooltip_plugin/fixtures/fixtures.ts
+++ tooltip_plugin/src/dp_tooltip_plugin/fixtures/fixtures.ts
@@ -5,6 +5,6 @@
  * These static constants are just examples for the plugin examples gallery usage. 
  */
 export const TOOLTIP_DATA = {
-    "aacWRQ4T37I6": "Example of the plugin providing KPIs with the tooltips.",
-    "aaoWFQCuOGLs": "Example of the plugin providing Insights with the tooltips.",
+    "example_kpi_identifier": "Example of the plugin providing KPIs with the tooltips.",
+    "example_insight_identifier": "Example of the plugin providing Insights with the tooltips.",
 }
