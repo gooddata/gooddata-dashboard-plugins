3d2d9d622769da434586b3b6f94e4c973c9aa7cb
Matyas Kandl <matyas.kandl@gooddata.com>
2022-11-01 14:31:31 +0100

    Single select plugin - make tiger backend default
    - change the default backend to tiger backend implementation
    
    JIRA: RAIL-4546

--- single_select_plugin/src/harness/Root.tsx
+++ single_select_plugin/src/harness/Root.tsx
@@ -2,12 +2,12 @@
 import React from "react";
 import { BackendProvider, WorkspaceProvider } from "@gooddata/sdk-ui";
 
-import { backend, hasCredentialsSetup, needsAuthentication } from "./backend";
+import { backend, hasCredentialsSetup } from "./backend";
 import { App } from "./App";
 import { DEFAULT_WORKSPACE } from "./constants";
 
 export const Root: React.FC = () => {
-    if (!hasCredentialsSetup() && needsAuthentication()) {
+    if (!hasCredentialsSetup()) {
         return (
             <div>
                 The environment is not setup with credentials to use for authentication to Analytical Backend.
