ef54da5c97998a886083be522e2d9e0c05fc3bc4
Matyas Kandl <matyas.kandl@gooddata.com>
2022-11-01 14:26:49 +0100

    Single select plugin - remove unused methods
    - remove unused methods in the plugin code (onPluginLoaded and onPluginUnload)
    
    JIRA: RAIL-4546

--- single_select_plugin/src/dp_single_select_plugin/Plugin.tsx
+++ single_select_plugin/src/dp_single_select_plugin/Plugin.tsx
@@ -11,20 +11,6 @@ export class Plugin extends DashboardPluginV1 {
     public readonly minEngineVersion = entryPoint.minEngineVersion;
     public readonly maxEngineVersion = entryPoint.maxEngineVersion;
 
-    public onPluginLoaded(_ctx: DashboardContext, _parameters?: string): Promise<void> | void {
-        /*
-         * This will be called when the plugin is loaded in context of some dashboard and before
-         * the register() method.
-         *
-         * If the link between the dashboard and this plugin is parameterized, then all the parameters will
-         * be included in the parameters string.
-         *
-         * The parameters are useful to modify plugin behavior in context of particular dashboard.
-         *
-         * Note: it is safe to delete this stub if your plugin does not need any specific initialization.
-         */
-    }
-
     public register(_ctx: DashboardContext, customize: IDashboardCustomizer): void {
         customize
             .filters()
@@ -32,14 +18,5 @@ export class Plugin extends DashboardPluginV1 {
             .withCustomProvider(() => {
                 return PluginAttributeFilter;
             });
-    }
-
-    public onPluginUnload(_ctx: DashboardContext): Promise<void> | void {
-        /*
-         * This will be called when user navigates away from the dashboard enhanced by the plugin. At this point,
-         * your code may do additional teardown and cleanup.
-         *
-         * Note: it is safe to delete this stub if your plugin does not need to do anything extra during unload.
-         */
     }
 }
