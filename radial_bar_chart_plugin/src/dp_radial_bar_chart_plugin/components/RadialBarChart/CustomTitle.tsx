71b414af320c6d57b000bbb5c85b77af1f5299f6
Kirill Kazakov <kazakovkirilliy@gmail.com>
2023-12-06 13:48:58 +0100

    fix(radial_bar_chart): change rendering logic to fix pdf export

---
+++ radial_bar_chart_plugin/src/dp_radial_bar_chart_plugin/components/RadialBarChart/CustomTitle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CustomTitle: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+    return (
+        <div className="item-headline-outer">
+            <div className="item-headline">
+                <div className="item-headline-inner s-headline">{children}</div>
+            </div>
+        </div>
+    );
+};
