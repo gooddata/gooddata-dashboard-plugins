b5fe136731d195c91fcda1ef799b72a3c3737621
František Német <frabtisejnemet@Frantisek--MacBook-Pro.local>
2025-07-28 09:29:50 +0200

    Remove ESM build files from Git tracking and update gitignore

--- polar_area_chart_plugin/esm/dashboardPlugin/2505.mjs
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkpolar_area_chart_plugin=self.webpackChunkpolar_area_chart_plugin||[]).push([[2505],{62505:(e,a,l)=>{Object.defineProperty(a,"__esModule",{value:!0});const n=l(38865);a.default=n.newDashboardEngine}}]);
