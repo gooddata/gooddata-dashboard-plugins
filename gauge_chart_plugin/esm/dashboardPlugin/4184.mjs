61ac2eadc81c81a0fc20d92844cf5a3277f5b2f7
DeepBlue321 <frantisek1nemet@gmail.com>
2025-09-30 09:29:20 +0200

    fix: update types node to 22 plus
    ND-457

--- gauge_chart_plugin/esm/dashboardPlugin/4184.mjs
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgauge_chart_plugin=self.webpackChunkgauge_chart_plugin||[]).push([[4184],{64184:function(e,n,u){Object.defineProperty(n,"__esModule",{value:!0});const a=u(38865);n.default=a.newDashboardEngine}}]);
